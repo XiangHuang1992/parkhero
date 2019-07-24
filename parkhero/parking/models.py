715515a9c8c937fbf1a92f584db29e10041fec0e
huangxiang <huangxiang1992@hotmail.com>
2019-07-24 22:27:50 +0800

    add parking module:新增停车场模块

---
+++ parkhero/parking/models.py
@@ -0,0 +1,26 @@
+from django.contrib.auth import get_user_model
+from django.db import models
+
+# Create your models here.
+
+User = get_user_model()
+
+
+class ParkingLot(models.Model):
+    pass
+
+
+class ParkingLotExtra(models.Model):
+    pass
+
+
+class VehicleIn(models.Model):
+    pass
+
+
+class VehicleOut(models.Model):
+    pass
+
+
+class ParkingGate(models.Model):
+    pass
