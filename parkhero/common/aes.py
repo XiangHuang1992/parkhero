730f099dca8c170bc10532fe8c8f3d6dec8bede9
huangxiang <huangxiang1992@hotmail.com>
2019-07-25 11:56:02 +0800

    add aes encrypt with base64

--- parkhero/common/aes.py
+++ parkhero/common/aes.py
@@ -10,3 +10,30 @@
 # Life is Short I Use Python!!!                      ===
 # ======================================================
 """
+import  base64
+
+from crypto.Cipher import AES
+
+
+BS = AES.block_size
+pad = lambda s: s + (BS - len(s) % BS) * chr(BS - len(s) % BS)
+unpad = lambda s: s[0:-ord(s[-1])]
+
+
+class Aes:
+    key = None
+    encryptor = None
+
+    def __int__(self):
+        self.mode = AES.MODE_CBC
+
+    def specifykey(self, key):
+        self.key = key
+        self.encryptor = AES.new(self.key)
+
+    def encrypt(self, plaintext):
+        return base64.b64encode(self.encryptor.encrypt(pad(plaintext)))
+
+    def decrypt(self, ciphertext):
+        un64_text = base64.b64decode(ciphertext)
+        return unpad(self.encryptor.decrypt(un64_text).decode("utf-8"))
