c1d0aa4a4ab1588507fe5c06fe52ddad4abf5e65
huangxiang <huangxiang1992@hotmail.com>
2019-07-29 09:30:08 +0800

    用户角色、群组检查

--- parkhero/common/usercheck.py
+++ parkhero/common/usercheck.py
@@ -24,14 +24,21 @@ logger = logging.getLogger(__name__)
 
 
 class UserCheck:
-
+    @staticmethod
     def login_check(request):
         """
         todo: diff anonymous user and login user
         :return:
         """
-        pass
+        logger.info("login as user[%s]", request.user)
+        if not request.user.is_authenticated():
+            detail = dict()
+            detail["detail"] = "Please login"
+            detail["status"] = STATUS_CODE["need_login"]
+            return 1, detail
+        return 0, None
 
+    @staticmethod
     def group_check(user, groupname):
         """
         todo: verify user whether not belong so specify group
@@ -39,19 +46,45 @@ class UserCheck:
         :param groupname:
         :return:
         """
-        pass
+        role_list = [i["name"] for i in user.groups.values()]
+        logger.info("Role list[%s]", role_list)
+        if groupname not in role_list:
+            logger.error("Please login as %s.", groupname)
+            detail = dict()
+            detail["detail"] = {"Please login as %s." % groupname}
+            detail["status"] = STATUS_CODE["non_right"]
+            return 1, detail
+        return 0, role_list
 
-    def auth_check(self):
+    @staticmethod
+    def auth_check(request, groupname):
         """
         todo: verify user whether not login and belong to specify group
         :return:
         """
-        pass
+        ret_val, ret_detail = UserCheck.login_check(request)
+        if ret_val != 0:
+            return 1, ret_detail
+        ret_val, ret_detail = UserCheck.group_check(request.user, groupname)
 
+        if ret_val != 0:
+            return 2, ret_detail
+
+        return 0, ret_detail
+
+    @staticmethod
     def app_user_login(user):
         """
         todo: verify whether not app user
         :return:
         """
-        pass
+        role_list = [i["name"] for i in user.groups.values()]
 
+        _is_app_user = False
+
+        if isinstance(role_list, list):
+            _is_app_user = not role_list or (
+                role_list and "default" not in role_list
+            )
+
+        return _is_app_user
