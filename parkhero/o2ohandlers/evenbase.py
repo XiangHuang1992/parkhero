258561c7728284b720302d5f9d481c55fd7ae000
huangxiang <huangxiang1992@hotmail.com>
2019-07-29 09:02:11 +0800

    创建o2ohandler模块

---
+++ parkhero/o2ohandlers/evenbase.py
@@ -0,0 +1,18 @@
+# -*- encoding: utf-8 -*-
+"""
+# @version: 0.1
+# @author: ferdinand
+# @email: ferinandhx@gmail.com
+# @flie: evenbase.py
+# @ide: PyCharm
+# @time: 2019-07-25 17:29
+# @desc: ===============================================
+# Life is Short I Use Python!!!                      ===
+# ======================================================
+"""
+import json
+from json.decoder import JSONDecodeError
+from abc import ABCMeta, abstractmethod
+
+from django.db import DatabaseError
+
